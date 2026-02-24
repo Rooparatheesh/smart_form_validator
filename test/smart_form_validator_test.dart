069103a8e80a29ebe98d661d2c4e642e65d1f126
unknown <Roopa Ratheesh>
2026-02-24 16:46:12 +0530

    Rename package to flutter_smart_validator

--- test/smart_form_validator_test.dart
+++ test/smart_form_validator_test.dart
@@ -1,5 +1,5 @@
+import 'package:flutter_smart_validator/smart_form_validator.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:smart_form_validator/smart_form_validator.dart';
 
 void main() {
   group('RequiredValidator Tests', () {
